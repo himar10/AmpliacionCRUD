281c468d7bad8a5f92ca2cf33b62eb62b06f37c8
himar10 <35796951+himar10@users.noreply.github.com>
2019-02-11 12:44:45 +0000

    Solucion de errores Crear

--- crear.php
+++ crear.php
@@ -6,15 +6,15 @@
 
     <form method="post" action="crear.php">
         Nombre:<br>
-        <input type="text" name="nombre" value="">
+        <input type="text" name="nombre" value="" required>
         <br>Apellido:<br>
-        <input type="text" name="apellido" value="">
+        <input type="text" name="apellido" value="" required>
         <br>DNI<br>
-        <input type="text" name="dni" value="">
+        <input type="text" name="dni" value="" required>
         <br>Email<br>
-        <input type="text" name="email" value="">
+        <input type="text" name="email" value="" required>
         <br>Fecha Nacimiento<br>
-        <input type="text" name="fecha" value="">
+        <input type="text" name="fecha" value="" required>
         <br><br>
         <input type="submit" value="Submit">
     </form>
@@ -26,25 +26,46 @@
 <?php
 
 require 'database.php';
+   if( isset($_POST['nombre']) ) {
+        $nombre = $_POST['nombre'];
+    }
+    if( isset($_POST['apellido']) ) {
+        $apellido = $_POST['apellido'];
+    }
+    if( isset($_POST['dni']) ) {
+        $dni = $_POST['dni'];
+    }
+    if( isset($_POST['email']) ) {
+        $email = $_POST['email'];
+    }
+    if( isset($_POST['fecha']) ) {
+        $fecha = $_POST['fecha'];
+    }
 
+
+/*
 $nombre = $_POST['nombre'];
 $apellido = $_POST['apellido'];
 $dni = $_POST['dni'];
 $email = $_POST['email'];
 $fecha = $_POST['fecha'];
+*/
 
+if (isset($nombre,$apellido, $dni, $email, $fecha)){
+    $pdo = Database::connect();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$pdo = Database::connect();
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$sql = "INSERT INTO persona (Nombre,Apellidos,DNI,fNacimiento,Correo) values('" . $nombre . "' , '"  . $apellido . "' , '"  . $dni . "' , '"  .  $fecha  . "' , '" . $email ."')";
-try {
-    $q = $pdo->prepare($sql);
-    $q->execute();
-} catch (Exception $e) {
-    echo ($e);
+    $sql = "INSERT INTO persona (Nombre,Apellidos,DNI,fNacimiento,Correo) values('" . $nombre . "' , '"  . $apellido . "' , '"  . $dni . "' , '"  .  $fecha  . "' , '" . $email ."')";
+    try {
+        $q = $pdo->prepare($sql);
+        $q->execute();
+    } catch (Exception $e) {
+        echo ($e);
+    }
+    Database::disconnect();
 }
-Database::disconnect();
+
+
 
 ?>
 
